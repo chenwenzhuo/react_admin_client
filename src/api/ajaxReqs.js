480191f6ea9deb9d63a4ca767d09652fc62a9c12
chenwenzhuo <Chenwz1996@163.com>
2022-07-26 23:33:13 +0800

    由于配置了代理，请求函数中的路径需要加上前缀

--- src/api/ajaxReqs.js
+++ src/api/ajaxReqs.js
@@ -1,6 +1,7 @@
 /*
 * 包含应用中所有接口请求函数的模块
 * 每个函数的返回值都是Promise对象
+* 由于配置了代理，请求路径中需要加上/ajaxProxy前缀
 */
 import ajax from './ajax'
 
@@ -8,9 +9,9 @@ import ajax from './ajax'
  * @param username 用户名，字符串类型
  * @param password 密码，字符串类型
  * */
-export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST');
+export const reqLogin = (username, password) => ajax('/ajaxProxy/login', {username, password}, 'POST');
 
 /**添加用户
  * @param user 用户信息对象
  * */
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST');
+export const reqAddUser = (user) => ajax('/ajaxProxy/manage/user/add', user, 'POST');
