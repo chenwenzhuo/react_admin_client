d234943212d39a7516c1c70489615bb95c5ad11d
chenwenzhuo <Chenwz1996@163.com>
2022-07-27 22:48:16 +0800

    自定义storageUtils持久化保存登陆用户的信息

---
+++ src/utils/storageUtils.js
@@ -0,0 +1,21 @@
+/*
+* 持久化保存登陆用户信息
+*/
+import store from 'store';
+
+const USER_KEY = 'login_user';
+
+export default {
+    // 保存登陆用户
+    saveLoginUser(user) {
+        store.set(USER_KEY, user);
+    },
+    // 获取登陆用户
+    getLoginUser() {
+        return store.get(USER_KEY) || {};
+    },
+    // 删除登陆用户
+    removeLoginUser() {
+        store.remove(USER_KEY);
+    }
+}
