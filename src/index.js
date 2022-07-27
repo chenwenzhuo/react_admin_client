9f57dc39bdce99bb590fa9df0cf2e80711fea7c8
chenwenzhuo <Chenwz1996@163.com>
2022-07-27 22:49:07 +0800

    应用启动时在持久化存储中读取登陆用户信息并保存到内存中

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import memoryUtils from './utils/memoryUtils';
+import storageUtils from './utils/storageUtils';
+
+//从持久存储中获取登陆用户信息，存入内存
+const loginUser = storageUtils.getLoginUser();
+memoryUtils.user = loginUser;
+
 // 将App组件标签渲染到index.html页面的id为root的元素上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
