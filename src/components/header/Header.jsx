dd5707b051e1beff46380027fb870c49d42a8bfe
chenwenzhuo <Chenwz1996@163.com>
2022-08-01 22:44:58 +0800

    完成Header组件静态界面

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,18 @@ class Header extends Component {
     render() {
         return (
             <div className='header'>
-                Header
+                <div className="header-top">
+                    <span>欢迎，admin</span>
+                    <button>退出</button>
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">首页</div>
+                    <div className="header-bottom-right">
+                        <span className="header-bottom-right-time">2022-8-1&nbsp;21:35:58</span>
+                        <img src="https://api.map.baidu.com/images/weather/day/qing.png" alt="weather"/>
+                        <span className="header-bottom-right-weather">晴</span>
+                    </div>
+                </div>
             </div>
         );
     }
