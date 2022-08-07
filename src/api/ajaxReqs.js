d6f749a652f9265b5d07968e67f8ab4549da3217
chenwenzhuo <Chenwz1996@163.com>
2022-08-07 14:28:41 +0800

    添加了查询分类列表、添加分类、更新分类名称的请求接口

--- src/api/ajaxReqs.js
+++ src/api/ajaxReqs.js
@@ -16,6 +16,34 @@ export const reqLogin = (username, password) => ajax('/ajaxProxy/login', {userna
  * */
 export const reqAddUser = (user) => ajax('/ajaxProxy/manage/user/add', user, 'POST');
 
+/**获取一级/二级分类列表
+ * @param parentId 父分类编号。查询一级分类时，由于其没有父分类，parentId为0
+ * */
+export const reqCategories = (parentId) => ajax('ajaxProxy/manage/category/list', {parentId});
+
+/**添加分类
+ * @param parentId 父分类编号。添加一级分类时，由于其没有父分类，parentId为0
+ * @param categoryName 待添加的分类名称
+ * */
+export const reqAddCategory = (parentId, categoryName) =>
+    ajax('ajaxProxy/manage/category/list', {
+        parentId,
+        categoryName
+    }, 'POST');
+
+/**更新分类名称
+ * @param categoryId 待更新待分类待id值（由后端生成，查询可得）
+ * @param newCategoryName 分类待新名称
+ * */
+export const reqUpdateCategory = (categoryId, newCategoryName) =>
+    ajax('ajaxProxy/manage/category/list', {
+        categoryId,
+        newCategoryName
+    }, 'POST');
+
+/**请求天气数据
+ * @param district_id 地区编号
+ * */
 export const reqWeather = (district_id) => ajax('/weatherProxy/weather/v1/', {
     district_id: district_id,
     data_type: 'all',
