cc02d4864827db4ada5faf2c06f2a7eedca26e05
chenwenzhuo <Chenwz1996@163.com>
2022-08-08 22:49:57 +0800

    搭建了Products组件的整体路由结构，包含ProdHome、ProdAddUpdate、ProdDetail子组件

--- src/pages/product/products.jsx
+++ src/pages/product/products.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {Switch, Route} from "react-router-dom";
+
+import ProdHome from "./prod-home";
+import ProdAddUpdate from "./prod-add-update";
+import ProdDetail from "./prod-detail";
 
 class Products extends Component {
     render() {
         return (
-            <div>
-                Products
-            </div>
+            <Switch>
+                {/*添加exact属性，使得在路由匹配时，目标路由路径必须与path值完全匹配，才进行路由跳转*/}
+                {/*否则，无法访问到二级路由页面add_update和detail*/}
+                {/*因为目标路由路径/products/add_update在匹配时，其中的/products先与path="/products"匹配成功，
+                   再在ProdHome组件的子路由组件中去匹配/add_update，故无法匹配成功*/}
+                <Route path="/products" component={ProdHome} exact/>
+                <Route path="/products/add_update" component={ProdAddUpdate} exact/>
+                <Route path="/products/detail" component={ProdDetail} exact/>
+            </Switch>
         );
     }
 }
