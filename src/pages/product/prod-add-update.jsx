ff06e7b35e2696207dde0b821ca75cf97a46c7ce
chenwenzhuo <Chenwz1996@163.com>
2022-08-08 22:48:37 +0800

    Products组件-添加、更新商品信息的子路由组件

---
+++ src/pages/product/prod-add-update.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// Products组件-添加、更新商品信息的子路由组件
+class ProdAddUpdate extends Component {
+    render() {
+        return (
+            <div>
+                Product Add Update
+            </div>
+        );
+    }
+}
+
+export default ProdAddUpdate;
