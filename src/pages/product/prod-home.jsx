e11367302641d89ed5e8ba0b2f9d8470b548b3f8
chenwenzhuo <Chenwz1996@163.com>
2022-08-08 22:48:18 +0800

    Products组件-默认子路由组件，展示商品信息

---
+++ src/pages/product/prod-home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// Products组件-默认子路由组件，用于展示商品信息
+class ProdHome extends Component {
+    render() {
+        return (
+            <div>
+                Product Home
+            </div>
+        );
+    }
+}
+
+export default ProdHome;
