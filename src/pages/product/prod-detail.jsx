f397fdafd3bf85db76ab40cfe935e0b485cdc6c4
chenwenzhuo <Chenwz1996@163.com>
2022-08-08 22:48:49 +0800

    Products组件-商品详情子路由组件

---
+++ src/pages/product/prod-detail.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// Products组件-商品详情子路由组件
+class ProdDetail extends Component {
+    render() {
+        return (
+            <div>
+                Product Detail
+            </div>
+        );
+    }
+}
+
+export default ProdDetail;
