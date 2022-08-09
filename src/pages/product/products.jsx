80a1910dc09996af867d6960d14696c430ec1977
chenwenzhuo <Chenwz1996@163.com>
2022-08-09 22:01:04 +0800

    修改路径严格匹配规则，更新/详情界面无需严格匹配，将无法匹配路径重定向到首页

--- src/pages/product/products.jsx
+++ src/pages/product/products.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 
 import ProdHome from "./prod-home";
 import ProdAddUpdate from "./prod-add-update";
@@ -14,8 +14,10 @@ class Products extends Component {
                 {/*因为目标路由路径/products/add_update在匹配时，其中的/products先与path="/products"匹配成功，
                    再在ProdHome组件的子路由组件中去匹配/add_update，故无法匹配成功*/}
                 <Route path="/products" component={ProdHome} exact/>
-                <Route path="/products/add_update" component={ProdAddUpdate} exact/>
-                <Route path="/products/detail" component={ProdDetail} exact/>
+                {/*add_update和detail未开启严格匹配，故/products/add_update/xxx等路径也可匹配成功，进入更新/详情界面*/}
+                <Route path="/products/add_update" component={ProdAddUpdate}/>
+                <Route path="/products/detail" component={ProdDetail}/>
+                <Redirect to={"/products"}/>{/*无法匹配的路径都重定向到首页*/}
             </Switch>
         );
     }
